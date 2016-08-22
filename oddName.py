ab4b3eb421d9da72e01c91dc350b59a0d6a44194
Marcus-Rogers <marcus.rogers@my.jcu.edu.au>
2016-08-22 12:59:37 +1000

    Add odd-lettered name printing functionality

--- oddName.py
+++ oddName.py
@@ -1,1 +1,7 @@
 """Marcus Rogers"""
+
+user_name = input("Please enter your name: ")
+while user_name == "" or user_name == " ":
+    user_name = input("Name cannot be blank. Please enter a valid name: ")
+
+print(user_name[1::2])
