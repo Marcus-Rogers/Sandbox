3f5fe1308a244b1c7df7d09342d8c55de1a7a428
Marcus-Rogers <marcus.rogers@my.jcu.edu.au>
2017-03-16 13:27:07 +1000

    Completed first CP1404 test of 2017

---
+++ firstTest.py
@@ -0,0 +1,19 @@
+# Question 7
+x = 5
+y = 10
+if x < 10:
+    if y >= 10 and x >= 5:
+        print("A")
+else:
+    print("B")
+
+# Question 9
+x = 10
+while x > 4:
+    print(x, end=" ")
+    x = x - 2
+print("\n")
+
+# Question 10
+for i in range(1, 10, 4):
+    print(i, i * 2, end=" ")
